de3cf460c21f31a7e76b2a82e36f286fc42ce489
Mark Fawcett <markfawcett@Marks-MacBook-Pro-2.local>
2020-06-29 23:00:34 +0100

    added python bin to path

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,1 @@
-set -x fish_user_paths  /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin ~/scripts
+set -x fish_user_paths  /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin ~/scripts /Library/Frameworks/Python.framework/Versions/3.8/bin
