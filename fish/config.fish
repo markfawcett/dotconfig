ce1e2fdf0e80415f7450c118b3ebde04741de3a4
Mark Fawcett <markjfawcett@gmail.com>
2020-07-03 13:11:13 +0100

    Added Oxygen PDF chemistry to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,3 @@
-set -x fish_user_paths  /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin ~/scripts
+
+# this is how we change the PATH in fish
+set -x fish_user_paths  /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin ~/scripts ~/Applications/oxygen-pdf-chemistry-22.1
