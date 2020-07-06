e2d4a4ed490b8c3e50fdde48f954fafc54c6e425
Mark Fawcett <markjfawcett@gmail.com>
2020-07-06 16:22:19 +0100

    added fundle and pipenv

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
+# fundle stuff
+fundle plugin 'sentriz/fish-pipenv'
+
+fundle init
+
 
 # this is how we change the PATH in fish
 set -x fish_user_paths  /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin ~/scripts /Library/Frameworks/Python.framework/Versions/3.8/bin ~/Applications/oxygen-pdf-chemistry-22.1
